6dd8bcd5f67da62844d665323933f52ea69ddb5e
Shinya Maeyama <merikonjatta@gmail.com>
2013-02-04 19:28:48 +0900

    Fix sample run script

--- run/sample.rb
+++ run/sample.rb
@@ -1,13 +1,13 @@
 # This is a sample run script for Batchup.
 $: << File.dirname(__FILE__)
-require "batchup"
+require "../batchup"
 
 settings = {
   "svn_myrepo" => {
     :rotate => 10,
     :command => :rsync,
     :precommand => "svnadmin dump /home/marco/svn/repos/myrepo > /home/marco/dumps/svn_myrepo 2>/dev/null",
-    :postcommand => "trash /home/marco/dumps/svn_myrepo",
+    :postcommand => "rm -f /home/marco/dumps/svn_myrepo",
     :source => "/home/marco/dumps/svn_myrepo",
     :target => "/mnt/hgfs/naboo/Dumps/svn/myrepo",
     :conditions => lambda { Time.now.wday == 2 },
